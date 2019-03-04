d6baaa9576aa7c815bc023bd002df951bced9b96
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2019-03-04 14:53:48 +0100

    react setup, basic layout

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ const Footer = () => {
+  return (
+
+        <footer class="bg-dark text-white mt-5 p-4 text-center">
+                 Copyright &copy; 2018 Dev Connector
+        </footer>
+  
+  )
+}
+
+export default Footer; 
